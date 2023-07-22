cf8e93811310c8d9f2b3edfc8d29718f980019ac
Gaurav <gaurav.t2002@gmail.com>
2023-07-22 16:02:10 +0530

    Second commit

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -1,4 +1,4 @@
-var FINAL_COLOR = "green";
+var FINAL_COLOR = "black";
 
 // swap function util for sorting algorithms takes input of 2 DOM elements with .style.height feature
 function swap(el1, el2) {
